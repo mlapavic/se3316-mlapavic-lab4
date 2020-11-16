abccc4db1abc5dbc8738b0b9d8d1e88d0becb675
mlapavic <60083278+mlapavic@users.noreply.github.com>
2020-11-16 17:48:44 -0500

    Added the css component and get request for courses
    Added some basic css style sheet code. Furthermore, I added a get request function for courses, allowing for search by subject, subject and course code, and subject and course code and component.

--- angular-lab4/src/app/app.component.ts
+++ angular-lab4/src/app/app.component.ts
@@ -13,10 +13,45 @@ export class AppComponent {
   constructor(private http: HttpClient){ }
 
   getCourses(subjectInput,courseInput,componentInput){
+    while((document.getElementById("timetables")).firstChild ){
+      (document.getElementById("timetables")).removeChild((document.getElementById("timetables")).firstChild );
+    }
     console.log(subjectInput);
     console.log(courseInput);
     console.log(componentInput);
     
+    if(courseInput == "" && componentInput == ""){
+      this.http.get(`http://localhost:3000/api/courses/${subjectInput}`)
+      .subscribe(
+        (data:any[]) => {
+          console.log(data);
+          const l = document.getElementById('timetables');
+              data.forEach(e => {
+                  const item = document.createElement('li');
+                  item.appendChild(document.createTextNode(`${e.subject} ${e.catalog_nbr} ${e.className} 
+                  ${e.course_info[0].ssr_component} ${e.course_info[0].start_time} ${e.course_info[0].end_time}
+                  ${e.course_info[0].days}`));
+                  l.appendChild(item);
+              });
+        }
+      )
+    }else{
+      this.http.get(`http://localhost:3000/api/courses/${subjectInput}/${courseInput}/${componentInput}`)
+      .subscribe(
+        (data:any[]) => {
+          console.log(data);
+          const l = document.getElementById('timetables');
+              data.forEach(e => {
+                  const item = document.createElement('li');
+                  item.appendChild(document.createTextNode(`${e.subject} ${e.catalog_nbr} ${e.className} 
+                  ${e.course_info[0].ssr_component} ${e.course_info[0].start_time} ${e.course_info[0].end_time}
+                  ${e.course_info[0].days}`));
+                  l.appendChild(item);
+              });
+        }
+      )
+    }
+
   }
 
   createSchedule(scheduleInput){
